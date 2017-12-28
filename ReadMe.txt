21506ef38e97b0b575d0f59a8ee7b48c121cf1c6
caishu1995 <2687366516@qq.com>
2017-12-28 14:16:39 +0800

    first complete

---
+++ ReadMe.txt
@@ -0,0 +1,32 @@
+﻿编写时间：2017、11、24
+编写人  ：石婧宜
+
+
+baseTest.html
+	编写目的：
+		1、跨域访问网页，并通过xpath获得网页特定区域内容
+		2、验证各网页是否允许跨域与iframe
+	使用技术：
+		1、xpath
+	相关代码：
+		无
+
+getAndDispose.html
+	编写目的：
+		1、跨域访问网页，并通过xpath获得网页特定区域内容
+		2、将数据存入cookie
+	使用技术：
+		1、xpath
+	相关代码：
+		1、demo.js
+		2、jquery-1.10.2.min.js
+	注：
+		这个是在上个界面的基础之上封装而来
+
+messagePart1.html
+	编写目的：
+		1、界面间数据通信
+	使用技术：
+		1、postmessage
+	相关代码：
+		1、messagePart2.html
---
+++ demo.js
@@ -0,0 +1,103 @@
+﻿(function (exports) {
+    var obj = {};
+    exports.$PAndGHTML = obj;
+
+    ///给url发送请求并获得内容
+    ///url       : 请求路径
+    ///path      : 查询的XPath路径
+    ///timerCount:定义的计时时间
+    ///返回      ： 节点集合
+    obj.getAndFindFunction = function (url, path, timerCount) {
+        var nodes = null;//返回的结果集
+        //发送请求
+        sendRequestFunction(url, timerCount, function (data) {
+            var domParser = new DOMParser();
+            var xmlDocumentObj = domParser.parseFromString(data, 'text/html'); //将字符串转成XML Document 对象
+            nodes = getSpecialContent(xmlDocumentObj, path);            //查找节点并返回节点集
+        }, function (errorText) {
+            if (errorText == "createError") {
+                alert('抱歉，无法创建');
+            } else if (errorText == "error") {
+                alert('抱歉，请求错误');
+            } else if (errorText === 404) {
+                alert("路径请求错误");
+            }
+        });
+        return nodes;
+    }
+
+
+    ///根据url发送请求，获得内容，处理了超时、路径错误、error的情况
+    ///url             : 请求路径
+    ///timerCount      :定义的计时时间
+    ///callbackFunction:成功之后的回调函数
+    ///errorFunction   :发生错误时的处理函数 createError：无法创建，error：请求错误，其他非正常编码
+    function sendRequestFunction(url, timerCount, callbackFunction, errorFunction) {
+        //设置跨域访问请求对象
+        var xmlHttp = new XMLHttpRequest();
+        if ("withCredentials" in xmlHttp) { // "withCredentials"属性是XMLHTTPRequest2中独有的
+            xmlHttp.open('GET', url, false);
+        } else if (typeof XDomainRequest != "undefined") { // 检测是否XDomainRequest可用
+            xmlHttp = new XDomainRequest();
+            xmlHttp.open('GET', url);
+        } else {
+            errorFunction('createError');
+            return;
+        }
+
+        //根据设置的时间，设置计时器，防止超时
+        var timedout = false;
+        var timer = setTimeout(function () {
+            timedout = true;
+            xmlHttp.abort();
+        }, timerCount);
+
+        //状态改变时
+        xmlHttp.onreadystatechange = function () {
+            if (xmlHttp.readyState != 4) { return; }
+            if (timedout) { return; }
+            clearTimeout(timer);
+            if (xmlHttp.status === 200) {
+                callbackFunction(xmlHttp.responseText);
+            } else {
+                errorFunction(xmlHttp.status);
+            }
+        };
+        //出错时
+        xmlHttp.onerror = function () {
+            errorFunction('error');
+        };
+
+        try{
+            xmlHttp.send(null);
+        } catch (e) {
+            if (e.code == 19)
+                errorFunction('error');
+        }
+    }
+
+    ///获得XMLDocumentObj的特定内容
+    ///XMLDocumentObj: xmlDocument对象
+    ///path          : 查询的XPath路径
+    ///返回          ： 节点集合
+    function getSpecialContent(XMLDocumentObj, path) {
+        var nodeList = null;
+
+        if (window.ActiveXObject) {
+            // code for IE
+            XMLDocumentObj.setProperty("SelectionLanguage", "XPath");
+            nodeList = XMLDocumentObj.selectNodes(path);
+        } else if (document.implementation && document.implementation.createDocument) {
+            // code for Mozilla, Firefox, Opera, etc.
+            var nodes = XMLDocumentObj.evaluate(path, XMLDocumentObj, null, XPathResult.ANY_TYPE, null);
+            var result = nodes.iterateNext();
+            nodeList = [];
+            while (result) {
+                nodeList.push(result);
+                result = nodes.iterateNext();
+            }
+        }
+
+        return nodeList;
+    }
+})(this);
